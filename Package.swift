b197c197a65065dfa576bb280dd7e7f38a6e149b
MarvAkhmed <MarvAkhmed@stud.kpfu.ru>
2024-03-17 00:48:38 +0300

    adding snapKit Dependency

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPM",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftPM",
-            targets: ["SwiftPM"]),
+            targets: ["SwiftPM"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftPM"),
+            name: "SwiftPM",
+            dependencies: ["SnapKit"]
+        ),
+            
         .testTarget(
             name: "SwiftPMTests",
             dependencies: ["SwiftPM"]),
