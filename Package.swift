736138d51a62c7e7c1d5a95f7a0a20285b44a999
MarvAkhmed <MarvAkhmed@stud.kpfu.ru>
2024-03-17 05:41:01 +0300

    added Alamofire

--- Package.swift
+++ Package.swift
@@ -14,15 +14,17 @@ let package = Package(
         ),
     ],
     dependencies: [ 
-        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1")
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.9.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftPM",
-            dependencies: ["SnapKit"]
+            dependencies: ["SnapKit", "Alamofire"]
         ),
+      
             
         .testTarget(
             name: "SwiftPMTests",
