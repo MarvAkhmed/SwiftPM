563cc63813614916a4be5e5ef324d6c13d68aad3
MarvAkhmed <MarvAkhmed@stud.kpfu.ru>
2024-03-17 06:39:05 +0300

    added alamofire and sdwebImage

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
         ),
     ],
     dependencies: [ 
+        
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.9.0")
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.9.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.19.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftPM",
-            dependencies: ["SnapKit", "Alamofire"]
+            dependencies: ["SnapKit", "Alamofire", "SDWebImage"]
         ),
       
             
--- Sources/SwiftPM/SwiftPM.swift
+++ Sources/SwiftPM/SwiftPM.swift
@@ -2,4 +2,6 @@
 // https://docs.swift.org/swift-book
 
 import UIKit
-
+import Alamofire
+import SnapKit
+import SDWebImage
